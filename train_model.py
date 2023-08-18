2f8895cf316fd5480de10100354220bb84d62ccc
n3lly <nelsonmg43@gmail.com>
2023-08-18 09:43:57 +0300

    initial commit

---
+++ train_model.py
@@ -0,0 +1,96 @@
+import os
+import gc
+import torch
+import OmegaConf
+import argparse
+import nemo.collections.asr as nemo_asr
+
+from config import accelerator, EPOCHS, TOKENIZER, TOKENIZER_TYPE_CFG
+from pytorch_lightning import Trainer
+from nemo.utils import exp_manager
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config_path', required=True, type=str, help='config file path')
+parser.add_argument('--pretrained_model', required=True, type=bool, help='pretrained model from huggingface')
+parser.add_argument('--train_from_scratch', default=False, required=True, type=bool, help='load encoder weights and finetune new model')
+parser.add_argument("--experiment_folder", required=True, type=str, help="Destination folder where checkpoints and experiment logs will be saved")
+parser.add_argument("--experiment_name", required=True, type=str, help="Current experiment name")
+parser.add_argument("--audio_folder", default=None, required=False, type=str, help="Audio folder if different from destination folder")
+args = parser.parse_args()
+
+config = OmegaConf.load(args.config_path)
+
+# Initialize a Trainer for the Transducer model
+trainer = Trainer(devices=1,
+                accelerator=accelerator,
+                max_epochs=EPOCHS,
+                enable_checkpointing=False,
+                logger=False,
+                log_every_n_steps=5,
+                check_val_every_n_epoch=2)
+
+model = nemo_asr.models.EncDecRNNTBPEModel(cfg=config.model, trainer=trainer)
+pretrained_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(args.pretrained_model, trainer=trainer)
+
+if args.train_from_scratch:
+    model.encoder.load_state_dict(pretrained_model.encoder.state_dict(), strict=True)
+
+else:
+    model = pretrained_model
+    model.cfg.train_ds.sample_rate = 16000
+    model.cfg.validation_ds.sample_rate = 16000
+    model.cfg.test_ds.sample_rate = 16000
+
+    model.set_trainer(trainer)
+    model.setup_training_data(config.model.train_ds)
+    model.setup_validation_data(config.model.validation_ds)
+    model.setup_test_data(config.model.test_ds)
+
+    model.cfg.tokenizer.dir = TOKENIZER
+    model.cfg.tokenizer.type = TOKENIZER_TYPE_CFG
+
+
+model.summarize()
+# Prepare NeMo's Experiment manager to handle checkpoint saving and logging for us
+
+# Environment variable generally used for multi-node multi-gpu training.
+# In notebook environments, this flag is unnecessary and can cause logs of multiple training runs to overwrite each other.
+os.environ.pop('NEMO_EXPM_VERSION', None)
+
+exp_config = exp_manager.ExpManagerConfig(
+    exp_dir=args.experiment_folder,
+    name=args.experiment_name,
+    resume_if_exists=True,
+    resume_ignore_no_checkpoint=True,
+    checkpoint_callback_params=exp_manager.CallbackParams(
+        monitor="val_wer",
+        mode="min",
+        always_save_nemo=True,
+        save_best_model=True,
+    ),
+)
+
+exp_config = OmegaConf.structured(exp_config)
+
+logdir = exp_manager.exp_manager(trainer, exp_config)
+
+# try:
+#   from google import colab
+#   COLAB_ENV = True
+# except (ImportError, ModuleNotFoundError):
+#   COLAB_ENV = False
+
+# # Load the TensorBoard notebook extension
+# if COLAB_ENV:
+#     %reload_ext tensorboard
+#     %tensorboard --logdir /content/drive/MyDrive/CGIAR/Speech\ Recognition/experiments/LG-Conformer-Char-Model/
+# else:
+#     print("To use TensorBoard, please use this notebook in a Google Colab environment.")
+
+# Release resources prior to training
+gc.collect()
+
+if accelerator == 'gpu':
+  torch.cuda.empty_cache()
+
+trainer.fit(model)
